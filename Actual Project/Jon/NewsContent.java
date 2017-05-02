a7ad583d059b35a759c76f3305432258e997e923
jkunjummen <jonkunjummen@gmail.com>
2017-05-01 21:07:30 -0500

    Update NewsContent.java

--- Actual Project/Jon/NewsContent.java
+++ Actual Project/Jon/NewsContent.java
@@ -4,6 +4,16 @@ public enum NewsContent {
 	SOURCE, TOPIC, SUBJECT;
 	
 	public String toString() {
+		switch (this) {
+		case SOURCE:
+			return "Source";
+		case TOPIC:
+			return "Topic";
+		case SUBJECT:
+			return "Subject";
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 	
 }
