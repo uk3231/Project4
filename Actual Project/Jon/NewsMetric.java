df1d60d0085205293bf3b3e41fe45e166c70d55c
jkunjummen <jonkunjummen@gmail.com>
2017-05-01 21:08:47 -0500

    Update NewsMetric.java

--- Actual Project/Jon/NewsMetric.java
+++ Actual Project/Jon/NewsMetric.java
@@ -2,7 +2,15 @@
 public enum NewsMetric {
 	LENGTH, COUNT;
 	
-	public String toString() {
+	public String toString(){
+		switch(this){
+		case LENGTH:
+			return "Length";
+		case COUNT:
+			return "Length";
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
