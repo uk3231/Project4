84fdf7f04911540e6382da6f896cfdd7ccdede0e
jkunjummen <jonkunjummen@gmail.com>
2017-05-01 21:12:01 -0500

    Update and rename Actual Project/Jon/SubjectComparator.java to Actual Project/Jon/Finished/SubjectComparator.java

--- Actual Project/Jon/Finished/SubjectComparator.java
+++ Actual Project/Jon/Finished/SubjectComparator.java
@@ -5,5 +5,6 @@ public class SubjectComparator implements Comparator<NewsStory> {
 	public static final SubjectComparator SUBJECT_COMPARATOR = new SubjectComparator();
 
 	public int compare(NewsStory newsStory1, NewsStory newsStory2) {
+		return newsStory1.getSubject().compareTo(newsStory2.getSubject());
 	}
 }
