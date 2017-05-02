90694510f46cac66320c3b3c6bd4f7a1c88739c1
jkunjummen <jonkunjummen@gmail.com>
2017-05-01 21:05:32 -0500

    Update SortCriterion.java

--- Actual Project/Jon/SortCriterion.java
+++ Actual Project/Jon/SortCriterion.java
@@ -3,6 +3,20 @@ public enum SortCriterion {
 	SOURCE, TOPIC, SUBJECT, LENGTH, DATE_TIME;
 	
 	public String toString() {
+		switch (this) {
+		case SOURCE:
+			return "Source";
+		case TOPIC:
+			return "Topic";
+		case SUBJECT:
+			return "Subject";
+		case LENGTH:
+			return "Length";
+		case DATE_TIME:
+			return "Date/Time";
+		default:
+			throw new IllegalArgumentException();
+		}
 	}
 
 }
